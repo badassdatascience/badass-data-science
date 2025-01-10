febbe05848fc7090089c31d9df4040953ad298fa
Emily Williams <emily.marie.williams@protonmail.com>
2025-01-10 10:29:15 -0800

    Added feature allowing more days than today to be predicted.

--- badassdatascience/science/astronomy/calculate_solar_noon.py
+++ badassdatascience/science/astronomy/calculate_solar_noon.py
@@ -8,9 +8,9 @@ import datetime
 #
 # user settings
 #
-latitude = 33.1192
-longitude = -117.0864
-my_timezone = 'America/Los_Angeles'
+#latitude = 32.7157
+#longitude = -117.1611
+#my_timezone = 'America/Los_Angeles'
 
 #
 # calculate solar noon in UTC
@@ -31,11 +31,10 @@ def calculate_solar_noon_in_UTC(
     if utc_date != None:
         start = utc_date
 
-    
     sunrise = observer.previous_rising(sun, start = start)
     noon = observer.next_transit(sun, start = sunrise)
     noon_dt = noon.datetime()
-    return noon_dt
+    return noon_dt, (latitude, longitude)
 
 def adjust_timezone_from_UTC(
         datetime_in_UTC,
@@ -43,34 +42,24 @@ def adjust_timezone_from_UTC(
 ):
     dt_utc_aware = datetime_in_UTC.replace(tzinfo = pytz.utc)
     dt_tz = dt_utc_aware.astimezone(pytz.timezone(to_timezone))
-    return dt_tz
+    return dt_tz, to_timezone
 
-def convert_datetime_HMS_to_hours_in_decimal(datetime_hms):
-    hour = datetime_hms.hour
-    minute = datetime_hms.minute
-    second = datetime_hms.second
+#def convert_datetime_HMS_to_hours_in_decimal(datetime_hms):
+#    hour = datetime_hms.hour
+#    minute = datetime_hms.minute
+#    second = datetime_hms.second
+#
+#    seconds_in_minutes = second / 60.
+#    hour_as_decimal = hour + (minute / 60.) + (seconds_in_minutes / 60.)
+#    return hour_as_decimal
 
-    seconds_in_minutes = second / 60.
-    hour_as_decimal = hour + (minute / 60.) + (seconds_in_minutes / 60.)
-    return hour_as_decimal
-
-
-
-noon_UTC = calculate_solar_noon_in_UTC()
-noon_local = adjust_timezone_from_UTC(noon_UTC)
-
-start_date = datetime.date(2024, 1, 1)
-date_list = [start_date + datetime.timedelta(days = x) for x in range(0, 365 + 1)]
-
-solar_noon_HMS_list = [
-    adjust_timezone_from_UTC(calculate_solar_noon_in_UTC(utc_date = x)) for x in date_list
-]
+noon_utc, (lat, lon) = calculate_solar_noon_in_UTC()
+noon_local, timezone = adjust_timezone_from_UTC(noon_utc)
 
 
 
-solar_noon_decimal_list = [convert_datetime_HMS_to_hours_in_decimal(x) for x in solar_noon_HMS_list]
+print(noon_utc, lat, lon)
+print(noon_local, timezone)
 
-print(solar_noon_decimal_list[0:5])
-
-
+#print('Today\'s solar noon occurs at:', str(noon_local).split(' ')[1])
 
