65d69ca26b919e41f4f505bcb8c4872959552ce6
Emily Williams <emily.marie.williams@protonmail.com>
2025-02-10 09:13:08 -0800

    Removed this file.

--- badassdatascience/forecasting/deep_learning/pipeline_components/boilerplate.py
+++ badassdatascience/forecasting/deep_learning/pipeline_components/boilerplate.py
@@ -7,7 +7,7 @@ import os
 #
 # user settings
 #
-application_root_directory = os.environ['APP_HOME']
+application_root_directory = os.environ['BDS_HOME']
 
 #
 # set the path and environment
@@ -15,6 +15,11 @@ application_root_directory = os.environ['APP_HOME']
 sys.path.append(application_root_directory + '/badassdatascience/django')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'infrastructure.settings'
+
+#
+# in case we need to run this from a Jupyter notebook
+#
+os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = 'true'
 
 #
 # load Django 
