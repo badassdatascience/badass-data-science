e3b35c861f63dbac19879e2bc5d19c5858010eaf
Emily Williams <emily.marie.williams@protonmail.com>
2025-02-10 09:03:19 -0800

    Initial import.

---
+++ badassdatascience/forecasting/deep_learning/pipeline_components/boilerplate.py
@@ -0,0 +1,23 @@
+#
+# load system libraries
+#
+import sys
+import os
+
+#
+# user settings
+#
+application_root_directory = os.environ['APP_HOME']
+
+#
+# set the path and environment
+#
+sys.path.append(application_root_directory + '/badassdatascience/django')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'infrastructure.settings'
+
+#
+# load Django 
+#
+import django
+django.setup()
