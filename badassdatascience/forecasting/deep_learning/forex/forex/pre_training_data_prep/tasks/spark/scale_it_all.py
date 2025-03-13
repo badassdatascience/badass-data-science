Fixed a bug that would have caused a lookahead.

badassdatascience/forecasting/deep_learning/forex/forex/pre_training_data_prep/tasks/spark/scale_it_all.py
@@ -5,13 +5,13 @@ from pyspark.sql.types import FloatType
 
 from utilities.spark_session import get_spark_session
 
-def compute_mean_sans_nans(the_list):
-    return float(np.nanmean(the_list))
+def compute_mean_sans_nans(the_list, n_back):
+    return float(np.nanmean(the_list[0:n_back]))
 
 udf_compute_mean_sans_nans = f.udf(compute_mean_sans_nans, FloatType())
 
-def compute_std_sans_nans(the_list):
-    return float(np.nanstd(the_list))
+def compute_std_sans_nans(the_list, n_back):
+    return float(np.nanstd(the_list[0:n_back]))
 
 udf_compute_std_sans_nans = f.udf(compute_std_sans_nans, FloatType())
 
@@ -22,8 +22,8 @@ def compute_scaling_statistics_for_later_use(**config):
     for item in config['list_data_columns']:
         sdf_arrays = (
             sdf_arrays
-            .withColumn('mean_' + item, udf_compute_mean_sans_nans(f.col(item)))
-            .withColumn('std_' + item, udf_compute_std_sans_nans(f.col(item)))
+            .withColumn('mean_' + item, udf_compute_mean_sans_nans(f.col(item), f.lit(config['n_back'])))
+            .withColumn('std_' + item, udf_compute_std_sans_nans(f.col(item), f.lit(config['n_back'])))
         )
 
     sdf_arrays.write.mode('overwrite').parquet(config['directory_output'] + '/' + config['filename_scaling_stats'])