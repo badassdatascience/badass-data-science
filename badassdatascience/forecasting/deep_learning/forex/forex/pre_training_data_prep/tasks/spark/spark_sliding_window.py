90331b6aae145c2c8286e6cd8975766343874d48
Emily Williams <emily.marie.williams@protonmail.com>
2025-03-13 03:22:23 -0700

    Initial import.

---
+++ badassdatascience/forecasting/deep_learning/forex/forex/pre_training_data_prep/tasks/spark/spark_sliding_window.py
@@ -0,0 +1,89 @@
+import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
+
+import pyspark.sql.functions as f
+from pyspark.sql.types import ArrayType, FloatType, IntegerType
+
+n_back = 180
+n_forward = 30
+offset = 1
+n_step = 10
+
+# https://numpy.org/devdocs/reference/generated/numpy.lib.stride_tricks.sliding_window_view.html
+def make_sliding_window_float(
+    values_array,
+    n_back,
+    n_forward,
+    offset,
+    n_step,
+):
+    arr = np.array(values_array)
+    if len(arr) <= n_back + n_forward + offset:
+        return None  # clean this up
+    else:
+        swv = sliding_window_view(arr, n_back + n_forward)[::n_step]
+
+    to_return = []
+    for i in range(0, swv.shape[0]):
+        to_return.append([float(x) for x in swv[i]])
+    
+    return to_return
+
+def make_sliding_window_int(
+    values_array,
+    n_back,
+    n_forward,
+    offset,
+    n_step,
+):
+    arr = np.array(values_array)
+    if len(arr) <= n_back + n_forward + offset:
+        return None  # clean this up
+    else:
+        swv = sliding_window_view(arr, n_back + n_forward)[::n_step]
+
+    to_return = []
+    for i in range(0, swv.shape[0]):
+        to_return.append([int(x) for x in swv[i]])
+    
+    return to_return
+
+
+udf_make_sliding_window_float = f.udf(
+    make_sliding_window_float,
+    ArrayType(ArrayType(FloatType()))
+)
+
+udf_make_sliding_window_int = f.udf(
+    make_sliding_window_int,
+    ArrayType(ArrayType(IntegerType()))
+)
+
+#def find_too_short(
+#        df,
+#):
+
+#    n_back = 180
+#    n_forward = 30
+#    offset = 1
+#    column_name = 'timestamps_all_sorted_length'
+
+#    threshold = n_back + n_forward + offset
+#    return df.where(f.col(column_name) >= threshold)
+
+
+def do_sliding_window(df):
+    
+    item_list = ['return', 'volatility', 'volume', 'lhc_mean', 'sin', 'cos']
+
+    for item in item_list:
+        df = (
+            df
+            .withColumn('sw_' + item, udf_make_sliding_window_float(f.col(item + '_sorted')))
+        )
+    for item in item_list:
+        df = df.drop(item + '_sorted')
+
+    df = df.withColumn('sw_timestamps', udf_make_sliding_window_int(f.col('timestamps_all_sorted'))) #.drop('timestamps_all_sorted')
+
+    return df
