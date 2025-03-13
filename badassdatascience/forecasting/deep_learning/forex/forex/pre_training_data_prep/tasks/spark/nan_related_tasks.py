Have a proper scaling regime in place now. By 'proper' I mean that there is now longer the lookahead bug in the mean and standard deviation computation that preceeds the scaling.

badassdatascience/forecasting/deep_learning/forex/forex/pre_training_data_prep/tasks/spark/nan_related_tasks.py
@@ -44,16 +44,16 @@ udf_count_nans_in_array = f.udf(count_nans_in_array, IntegerType())
 
 
 ##https://stackoverflow.com/questions/41190852/most-efficient-way-to-forward-fill-nan-values-in-numpy-array
-#def forward_fill(values_array):
-#    arr = np.array([values_array])
-#    mask = np.isnan(arr)
-#    idx = np.where(~mask, np.arange(mask.shape[1]), 0)
-#    np.maximum.accumulate(idx, axis = 1, out = idx)
-#    arr[mask] = arr[np.nonzero(mask)[0], idx[mask]]
-#    to_return = list([float(x) for x in arr[0]])
-#    return to_return
-#
-#udf_forward_fill = f.udf(forward_fill, ArrayType(FloatType()))
+def forward_fill(values_array):
+    arr = np.array([values_array])
+    mask = np.isnan(arr)
+    idx = np.where(~mask, np.arange(mask.shape[1]), 0)
+    np.maximum.accumulate(idx, axis = 1, out = idx)
+    arr[mask] = arr[np.nonzero(mask)[0], idx[mask]]
+    to_return = list([float(x) for x in arr[0]])
+    return to_return
+
+udf_forward_fill = f.udf(forward_fill, ArrayType(FloatType()))
 
 
 def locate_nans(timestamp_array, timestamp_all_array, values_array):
@@ -180,4 +180,23 @@ def filter_by_nan_counts(**config):
     )
 
     sdf_arrays.write.mode('overwrite').parquet(config['directory_output'] + '/' + config['filename_post_nan_filters'])
+    spark.stop()
+
+
+def forward_fill_it_all(**config):
+    spark = get_spark_session(config['spark_config'])
+    sdf_arrays = spark.read.parquet(config['directory_output'] + '/' + config['filename_scaling_stats'])
+
+    items_list = []
+    items_list.extend(config['list_data_columns'])
+    items_list.extend(config['list_data_columns_no_scale'])
+
+    for item in items_list:
+        sdf_arrays = (
+            sdf_arrays
+            .withColumn(item + '_ff', udf_forward_fill(f.col(item)))
+            .drop(item)
+        )
+
+    sdf_arrays.write.mode('overwrite').parquet(config['directory_output'] + '/' + config['filename_forward_filled'])
     spark.stop()

badassdatascience/forecasting/deep_learning/forex/forex/pre_training_data_prep/tasks/spark/scale_it_all.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import pyspark.sql.functions as f
-from pyspark.sql.types import FloatType
+from pyspark.sql.types import ArrayType, FloatType
 
 from utilities.spark_session import get_spark_session
 
@@ -27,4 +27,28 @@ def compute_scaling_statistics_for_later_use(**config):
         )
 
     sdf_arrays.write.mode('overwrite').parquet(config['directory_output'] + '/' + config['filename_scaling_stats'])
+    spark.stop()
+
+
+
+def properly_scale(the_array, the_mean, the_std):
+    return [float(x) for x in (np.array(the_array) - the_mean) / the_std]
+
+udf_properly_scale = f.udf(properly_scale, ArrayType(FloatType()))
+
+def properly_scale_it_all(**config):
+    spark = get_spark_session(config['spark_config'])
+    sdf_arrays = spark.read.parquet(config['directory_output'] + '/' + config['filename_forward_filled'])
+
+    for item in config['list_data_columns']:
+        sdf_arrays = (
+            sdf_arrays
+            .withColumn(
+                item + '_ff_scaled',
+                udf_properly_scale(f.col(item + '_ff'), f.col('mean_' + item), f.col('std_' + item))
+            )
+            .drop(item + '_ff')
+        )
+
+    sdf_arrays.write.mode('overwrite').parquet(config['directory_output'] + '/' + config['filename_scaled'])
     spark.stop()