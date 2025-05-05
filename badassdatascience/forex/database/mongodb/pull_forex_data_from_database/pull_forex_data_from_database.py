2e3d4108c6d61ab2a5ea4709e9d841d1c56bd60b
Emily Williams <emily.marie.williams@protonmail.com>
2025-05-04 22:05:08 -0700

    Added a wrapper function...

--- badassdatascience/forex/database/mongodb/pull_forex_data_from_database/pull_forex_data_from_database.py
+++ badassdatascience/forex/database/mongodb/pull_forex_data_from_database/pull_forex_data_from_database.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import pickle
 
+from badassdatascience.forex.database.mongodb.utilities.connect_to_mongoDB import get_candlestick_documents
 
 #
 # we perform sorts in both MongoDB and Pandas, 
@@ -39,4 +41,23 @@ def pull_forex_into_df(candlesticks, instrument = 'EUR/USD', granularity = 'H1')
     df['time_iso_pd_datetime'] = [
         pd.to_datetime(q, format='%Y-%m-%dT%H:%M:%S%z') for q in df['time_iso']
     ]
+    return df
+
+
+def query_single_instrument_and_single_granularity(
+    instrument = 'EUR/USD',
+    granularity = 'M15',
+    pull_from_database = True,
+    save_file = 'output/df.pickled',
+):
+
+    if pull_from_database:
+        candlestick_documents = get_candlestick_documents()
+        df = pull_forex_into_df(candlestick_documents, instrument = instrument, granularity = granularity)
+        with open(save_file, 'wb') as fff:
+            pickle.dump(df, fff)
+    else:
+        with open(save_file, 'rb') as fff:
+            df = pickle.load(fff)
+
     return df
