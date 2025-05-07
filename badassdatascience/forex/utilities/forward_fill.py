d01a7c2e63c053b1863ed9a4b4180abeaa93b2b4
Emily Williams <emily.marie.williams@protonmail.com>
2025-05-06 19:38:31 -0700

    Added a visual test.

--- badassdatascience/forex/utilities/forward_fill.py
+++ badassdatascience/forex/utilities/forward_fill.py
@@ -1,4 +1,7 @@
 import itertools
+import datetime
+from pytz import timezone
+import matplotlib.pyplot as plt
 
 def ff(
     df,
@@ -16,3 +19,48 @@ def ff(
         df[column_name] = df_fill_columns[column_name]
 
     return df
+
+
+def test_post_ff(df_post_ff, timezone_name = 'US/Eastern'):
+    tz = timezone(timezone_name)
+    
+    # 16:00 rather than 17:00 because of daylight savings time
+    weekend_start_method_one = datetime.datetime(2025, 4, 25, 16, 0, 0, 0, tzinfo = tz).timestamp()
+    weekend_end_method_one = datetime.datetime(2025, 4, 27, 16, 0, 0, 0, tzinfo = tz).timestamp()
+
+    df_weekend_ff_test_one = df_post_ff[
+        (df_post_ff['time'] >= weekend_start_method_one) & 
+        (df_post_ff['time'] < weekend_end_method_one)
+    ]
+
+    # check immediate surroundings of the previous interval
+    weekend_start_method_two = datetime.datetime(2025, 4, 25, 15, 0, 0, 0, tzinfo = tz).timestamp()
+    weekend_end_method_two = datetime.datetime(2025, 4, 27, 17, 0, 0, 0, tzinfo = tz).timestamp()
+
+    df_weekend_ff_test_two = df_post_ff[
+        (df_post_ff['time'] >= weekend_start_method_two) & 
+        (df_post_ff['time'] < weekend_end_method_two)
+    ]
+
+    # final test
+    x_start = datetime.datetime(2025, 4, 24, 15, 0, 0, 0, tzinfo = tz).timestamp()
+    x_end = datetime.datetime(2025, 4, 30, 15, 0, 0, 0, tzinfo = tz).timestamp()
+    df_ff_final_test = df_post_ff[
+        (df_post_ff['time'] >= x_start) & 
+        (df_post_ff['time'] < x_end)
+    ]
+
+    return df_weekend_ff_test_one, df_weekend_ff_test_two, df_ff_final_test
+
+def rough_ff_plot(x, y):
+    plt.figure()
+    plt.plot(x, y)
+    plt.show()
+    plt.close()
+
+def ff_test_plots(df_post_ff):
+    df_weekend_ff_test_one, df_weekend_ff_test_two, df_ff_final_test = test_post_ff(df_post_ff)
+    
+    rough_ff_plot(df_weekend_ff_test_one['time'], df_weekend_ff_test_one['ask_c'])
+    rough_ff_plot(df_weekend_ff_test_two['time'], df_weekend_ff_test_two['ask_c'])
+    rough_ff_plot(df_ff_final_test['time'], df_ff_final_test['ask_c'])
