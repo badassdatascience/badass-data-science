681210cde9e0c1f67858fd8c2599c063208f33a5
Emily Williams <emily.marie.williams@protonmail.com>
2025-07-11 21:08:20 -0700

    Updated the nlags shown.

--- badassdatascience/utilities/badass_timeseries/acf_and_pacf.py
+++ badassdatascience/utilities/badass_timeseries/acf_and_pacf.py
@@ -16,13 +16,9 @@ def compute_pacf_the_way_emily_wants_it(x, alpha = 0.05, nlags = 49):
     cf, ci = pacf(x, alpha = alpha, nlags = nlags)
     return cf, ci
 
-def plot_acf_and_pacf_the_way_emily_wants_it(cf, CI, title, skip_lag_0 = True):
+def plot_acf_and_pacf_the_way_emily_wants_it(cf, CI, title, first_lag = 0):
     plt.figure()
 
-    first_lag = 0
-    if skip_lag_0:
-        first_lag = 1
-    
     plt.plot(np.arange(first_lag, len(cf)), cf[first_lag:], '.', color = 'blue')
     plt.axhline(0., color = 'black')
     plt.plot(np.arange(first_lag, len(cf)), CI[first_lag:, 0], color = 'orange')
